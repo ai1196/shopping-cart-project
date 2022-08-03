20e2c08c3e1a69fef440ad4ae60e04663a696ea2
Asha Isaac <ai1196@stern.nyu.edu>
2022-08-03 00:35:06 -0400

    Added date/time

--- shopping_cart.py
+++ shopping_cart.py
@@ -1,5 +1,7 @@
 # shopping_cart.py
 
+import datetime as dt
+
 products = [
     {"id":1, "name": "Chocolate Sandwich Cookies", "department": "snacks", "aisle": "cookies cakes", "price": 3.50},
     {"id":2, "name": "All-Seasons Salt", "department": "pantry", "aisle": "spices seasonings", "price": 4.99},
@@ -45,16 +47,17 @@ def to_usd(my_price):
 
 ## A grocery store name of your choice
 ## A grocery store phone number and/or website URL and/or address of choice
-# The date and time of the beginning of the checkout process, formatted in a human-friendly way (e.g. 2020-02-07 03:54 PM)
+## The date and time of the beginning of the checkout process, formatted in a human-friendly way (e.g. 2020-02-07 03:54 PM)
 # The name and price of each shopping cart item, price being formatted as US dollars and cents (e.g. $3.50, etc.)
 # The total cost of all shopping cart items (i.e. the "subtotal"), formatted as US dollars and cents (e.g. $19.47), calculated as the sum of their prices
 # The amount of tax owed (e.g. $1.70), calculated by multiplying the total cost by a New York City sales tax rate of 8.75% (for the purposes of this project, groceries are not exempt from sales tax)
 # The total amount owed, formatted as US dollars and cents (e.g. $21.17), calculated by adding together the amount of tax owed plus the total cost of all shopping cart items
-# A friendly message thanking the customer and/or encouraging the customer to shop again
+## A friendly message thanking the customer and/or encouraging the customer to shop again
 
 
 # Checkpoint 1: Capturing User Inputs
 
+checkout_time = dt.datetime.now()
 total_cost = 0
 chosen_ids = []
 
@@ -70,7 +73,7 @@ print("ASHA'S GROCERY STORE")
 print("----------------------------")
 print("Web: www.ashasgrocery.com")
 print("Phone: 123.456.7890")
-print("Checkout Time:")
+print("Checkout Time: " + checkout_time.strftime("%Y-%m-%d %I:%M %p")) 
 print("----------------------------")
 
 # print(chosen_ids)
@@ -90,4 +93,6 @@ for chosen_id in chosen_ids:
 
 print("TOTAL COST: " + str(total_cost))
 
+print("----------------------------")
 print("Thanks for shopping with us! Hope to see you soon.")
+print("----------------------------")
